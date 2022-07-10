3c732da8cb21e7847bc3885dbf8de45f744ff9ef
Lisa Aguilera <45641231+lisa-aguilera@users.noreply.github.com>
2022-07-10 14:52:11 -0700

    Update streamlit_app.py
    fixing error

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,6 @@ fruit_add = streamlit.text_input('What fruit would you like to add?','Jackfruit'
 streamlit.write('The user entered ', fruit_add)
 
 #testing
-my_cur.execute("insert into fruit_load_list values ('from streamlit');"
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
                
